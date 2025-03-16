eeee28aa2df49d3dff74b14d34fa7ba38e4f989b
bdelpozo <bdelpozo.del@gmail.com>
2025-03-16 14:24:01 +0100

    Añadir modulo fastapi con servicio apuntando a capa plata

--- src/services/fastapi_app/main.py
+++ src/services/fastapi_app/main.py
@@ -1,13 +1,30 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import pandas as pd
 import glob
 
-app = FastAPI()
+
+
+title = "Proyecto: MVP Análisis de Tickets de Supermercado"
+app = FastAPI(
+    version="1.0.0",
+    title=title
+    )
+
 
 @app.get("/tickets")
 def get_tickets():
-    parquet_files = glob.glob("/app/data/oro/*.parquet")
+    parquet_files = glob.glob("/app/data/plata/*.parquet")
     if not parquet_files:
-        return {"message": "No hay datos en la capa Oro"}
-    df_oro = pd.read_parquet(parquet_files[0])
-    return df_oro.to_dict(orient="records")
+        return {"message": "No hay datos en la capa Plata"}
+    df_plata = pd.read_parquet(parquet_files[0])
+    return df_plata.to_dict(orient="records")
+
+
+@app.get("/", include_in_schema=False)
+def redirigir():
+    return RedirectResponse(url="/docs")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
